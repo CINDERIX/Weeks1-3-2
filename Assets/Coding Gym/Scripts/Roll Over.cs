c30930f13ed111ee34adbeab3a7bca63598a7f16
songmarc <songmarc@OA-S141-06>
2026-01-12 11:51:27 -0500

    worked on roll over some more

--- Assets/Coding Gym/Scripts/Roll Over.cs
+++ Assets/Coding Gym/Scripts/Roll Over.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class RollOver : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool timer = false;
+    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    public float t = 0;
     void Start()
     {
         
@@ -11,6 +16,15 @@ public class RollOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        if (Vector2.Distance(mousePosition, new Vector2(transform.position.x, transform.position.y) <= ))
+        {
+            timer = true;
+        }
+
+        while (timer) { 
+            t += Time.deltaTime;
+        }
     }
 }
