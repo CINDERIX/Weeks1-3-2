cf54a44a351ccec12ede4a835e07e0eb10bc275c
songmarc <songmarc@OA-S141-06>
2026-01-19 10:02:28 -0500

    changed the sprite using an array and a random number

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -6,19 +6,25 @@ public class SpriteChanger : MonoBehaviour
 
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public Sprite[] barrels = new Sprite[3];
+
+    public int randomNumber = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //PickARandomColor();
+        //spriteRenderer.sprite = mySprite;
+        PickARandomSprite();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    PickARandomColor();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+            //PickARandomColor();
+            PickARandomSprite();
+        }
 
         //get the mouse position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -38,6 +44,27 @@ public class SpriteChanger : MonoBehaviour
     void PickARandomColor()
     {
         spriteRenderer.color = Random.ColorHSV();
+    }
+
+    void PickARandomSprite() 
+    {
+        //pick a random number
+        randomNumber = Random.Range(0, barrels.Length);
+        ////assign that sprite
+        //if (randomNumber == 0)
+        //{
+        //    spriteRenderer.sprite = barrel0;
+        //}
+        //else if (randomNumber == 1)
+        //{
+        //    spriteRenderer.sprite = barrel1;
+        //}
+        //else if (randomNumber == 2)
+        //{
+        //    spriteRenderer.sprite = barrel2;
+        //}   
+
+        spriteRenderer.sprite = barrels[randomNumber];
     }
 
 }
